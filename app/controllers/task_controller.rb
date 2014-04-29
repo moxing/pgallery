9bee2a72988d63e1c9bd4d35e10628714ecb5c5e
liu tao <star2ko@gmail.com>
2014-04-29 18:06:56 +0800

    task create

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,8 +1,28 @@
+require 'net/http'
+require 'nokogiri'
+
 class TaskController < ApplicationController
+
   def index
-  	
+  	if params[:url]
+  	  url = params[:url]
+  	  @list = getList(url)
+  	end
   end
 
   def history
   end
+
+  private
+
+  def getList( url )
+  	uri = URI.parse(url)
+  	list = Array.new
+  	doc = Nokogiri::HTML(Net::HTTP.get(uri))
+	doc.css('table td a img.border').each do |img|
+		list.push(img['src'])
+	end
+
+	return list
+  end  
 end
